15586a10aab5697a127d9ef565b7a09e513d78e7
yoBiro <bielzingg92@gmail.com>
2025-12-21 19:12:10 -0300

    feat: add NavItem component for NavBar

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,3 +1,5 @@
+import './navitem.js';
+
 class AppNavbar extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
@@ -13,41 +15,49 @@ class AppNavbar extends HTMLElement {
                 </div>
                 </div>
 
-                <span class="w-px h-8 bg-neutral-300"></span>
-
+                
                 <!-- CENTER -->
                 <div class="flex items-center gap-2">
-                    <img src="/src/assets/Icons/SocialMedia/YoutubeLogo.svg" class="w-7 h-7 opacity-70 hover:opacity-100 transition">
-                    <img src="/src/assets/Icons/SocialMedia/LinkedinLogo.svg" class="w-7 h-7 opacity-70 hover:opacity-100 transition">
-                    <img src="/src/assets/Icons/SocialMedia/GithubLogo.svg" class="w-7 h-7 opacity-70 hover:opacity-100 transition">
+                <a href="https://www.youtube.com/@santoslovetech"><img src="/src/assets/Icons/SocialMedia/YoutubeLogo.svg" class="w-7 h-7"></a>
+                <a href="https://www.linkedin.com/in/gabrielsdev"><img src="/src/assets/Icons/SocialMedia/LinkedinLogo.svg" class="w-7 h-7"></a>
+                <a href="https://github.com/yoBiro"><img src="/src/assets/Icons/SocialMedia/GithubLogo.svg" class="w-7 h-7"></a>
                 </div>
+                
+                <span class="w-px h-8 bg-neutral-300"></span>
 
                 <!-- RIGHT -->
-                <div class="flex items-center gap-2">
-                ${this.navItem('Home')}
-                ${this.navItem('About')}
-                ${this.navItem('Blog')}
-                ${this.navItem('Projects')}
-                </div>
+                    <div class="flex items-center gap-2">
+                    <nav-item
+                        label="Home" 
+                        href="/index.html"
+                        icon-src="https://img.icons8.com/?size=100&id=KM1nMFNMGbRI&format=png&color=000000">
+                    </nav-item>
+
+                        <nav-item
+                            label="About Me"
+                            href="/src/pages/about.html"
+                            icon-src="https://img.icons8.com/?size=100&id=N0kAkzTwlI3p&format=png&color=000000">
+                        </nav-item>
+
+                        <nav-item 
+                            label="Blog" 
+                            href="/blog"
+                            icon-src="https://img.icons8.com/?size=100&id=iGTqSjdkxMJw&format=png&color=000000">
+                        </nav-item>
+
+                        <nav-item 
+                            label="Projects" 
+                            href="/projects"
+                            icon-src="https://img.icons8.com/?size=100&id=2lzi4NM0vE21&format=png&color=000000">
+                        </nav-item>
+                    </div>
 
             </div>
             </div>
-        `;
-    }
-
-    navItem(label) {
-        return `
-        <a href="#"
-            class="flex items-center gap-2 px-4 py-1.5 rounded-full
-                    border border-neutral-300
-                    text-sm font-medium
-                    hover:bg-neutral-100 transition">
-            ${label}
-        </a>
         `;
     }
     }
 
     if (!customElements.get('app-navbar')) {
-    customElements.define('app-navbar', AppNavbar);
+        customElements.define('app-navbar', AppNavbar);
     }
