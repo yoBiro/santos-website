1f222850eb51f8c4602afbe6f2d10e9fa7df96b2
yoBiro <bielzingg92@gmail.com>
2025-12-23 10:11:04 -0300

    feat: add stack component and finishing cardstack. Add animation to index page load

--- src/components/Additionals/stack.js
+++ src/components/Additionals/stack.js
@@ -1,7 +1,17 @@
 class Stack extends HTMLElement {
     connectedCallback() {
+        const label = this.getAttribute('label') ?? '';
+        const iconSrc = this.getAttribute('icon-src');
+
         this.innerHTML = `
-            
+            <div class="flex flex-col items-center justify-center gap-2">
+                    <img src="${iconSrc}" class="w-24 h-24">
+                    <strong class="text-[1.25rem] font-mono font-medium">${label}</strong>
+            </div>
         `;
     }
+}
+
+if (!customElements.get('app-stack')) {
+    customElements.define('app-stack', Stack);
 }
--- src/components/Cards/cardstack.js
+++ src/components/Cards/cardstack.js
@@ -1,5 +1,6 @@
 import '/src/components/Buttons/primarybutton.js';
 import '/src/components/Buttons/secondarybutton.js';
+import '/src/components/Additionals/stack.js';
 
 class CardStack extends HTMLElement {
     connectedCallback() {
@@ -14,8 +15,16 @@ class CardStack extends HTMLElement {
                                 <app-secondarybutton label="View Blog" icon-src="https://img.icons8.com/?size=100&id=iGTqSjdkxMJw&format=png&color=000000"></app-secondarybutton>
                         </div>
                     </div>
-                        <div class="flex flex-row">
+                        <div class="flex flex-col gap-8">
                                 <strong class="text-[1.5rem]">Stack</strong>
+                                <div class="grid grid-cols-3 grid-rows-2 gap-6">
+                                    <app-stack label="Java" icon-src="/src/assets/Icons/Stack/java.svg"></app-stack>
+                                    <app-stack label="TypeScript" icon-src="/src/assets/Icons/Stack/typescript.svg"></app-stack>
+                                    <app-stack label="Linux" icon-src="/src/assets/Icons/Stack/linux.svg"></app-stack>
+                                    <app-stack label="PostgreSQL" icon-src="/src/assets/Icons/Stack/postgresql.svg"></app-stack>
+                                    <app-stack label="Figma" icon-src="/src/assets/Icons/Stack/figma.svg"></app-stack>
+                                    <app-stack label="Vercel" icon-src="/src/assets/Icons/Stack/vercel_dark.svg"></app-stack>
+                                </div>
                         </div>
                 </div>
             </div>
