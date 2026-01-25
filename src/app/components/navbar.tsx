ad923e14959d58c847967acd2d21da0126ea7055
yoBiro <bielzingg92@gmail.com>
2026-01-25 16:05:32 -0300

    fix: update fonts and navbar component

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,15 @@
+// app/components/Navbar.tsx
+export default function Navbar() {
+    return (
+        <div className="app-container flex items-center justify-between py-4">
+            <div>
+                <span className="text-2xl font-bold">SantosLoveTech</span>
+                <div className="mt-2 h-0.5 w-20 bg-neutral-300" />
+            </div>
+
+                <div className="flex gap-2">
+                    {/* botões */}
+                </div>
+            </div>
+        )
+}
